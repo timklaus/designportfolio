e95e56b64137607c364d9c47b2c98d6704b654e3
Tim Klaus <timklaus@Tims-MacBook-Pro.local>
2021-03-18 22:04:45 -0700

    fix jumpy scroll under sticky nav

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,16 @@
 $(document).ready(function() {
   
   $(window).scroll(function () { 
-      console.log($(window).scrollTop())
-    if ($(window).scrollTop() > 800) {
+      //console.log($(window).scrollTop())
+    if ($(window).scrollTop() > 799) {
       $('#navbar').addClass('sticky');
+      $('body').css('padding-top', '14.25vw'); 
+      $('#navmargin').addClass('topblock');
     }
-    if ($(window).scrollTop() < 801) {
+    if ($(window).scrollTop() < 800) {
       $('#navbar').removeClass('sticky');
+      $('body').css('padding-top', '');
+      $('#navmargin').removeClass('topblock');
     }
   });
 });
