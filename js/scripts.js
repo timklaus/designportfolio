de1a0b466fd18b23b002612a5edd2b4eb36ff834
Tim Klaus <timklaus@Tims-MacBook-Pro.local>
2021-06-14 15:45:37 -0700

    add jquery media query to attempt to fix mobile hero image issue

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,30 @@
 $(document).ready(function() {
-  
   $(window).scroll(function () { 
-      //console.log($(window).scrollTop())
-    if ($(window).scrollTop() > 999) {
-      $('#navbar').addClass('sticky');
-      $('body').css('padding-top', '8.25vw'); 
-      $('#navmargin').addClass('topblock');
+    if ($(window).width()<1366) {
+      console.log("fixed background attachment")
+      if ($(window).scrollTop() > 1999) {
+        $('#navbar').addClass('sticky');
+        $('body').css('padding-top', '8.25vw'); 
+        $('#navmargin').addClass('topblock');
+      }
+      if ($(window).scrollTop() < 2000) {
+        $('#navbar').removeClass('sticky');
+        $('body').css('padding-top', '');
+        $('#navmargin').removeClass('topblock');
+      }
     }
-    if ($(window).scrollTop() < 1000) {
-      $('#navbar').removeClass('sticky');
-      $('body').css('padding-top', '');
-      $('#navmargin').removeClass('topblock');
+    else {
+      console.log("fixed background attachment")
+      if ($(window).scrollTop() > 999) {
+        $('#navbar').addClass('sticky');
+        $('body').css('padding-top', '8.25vw'); 
+        $('#navmargin').addClass('topblock');
+      }
+      if ($(window).scrollTop() < 1000) {
+        $('#navbar').removeClass('sticky');
+        $('body').css('padding-top', '');
+        $('#navmargin').removeClass('topblock');
+      }
     }
   });
 
