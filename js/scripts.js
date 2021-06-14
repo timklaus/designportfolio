a93c6abb87caf8de52437b28b8b8f561310a8443
Tim Klaus <timklaus@Tims-MacBook-Pro.local>
2021-06-14 14:51:23 -0700

    change hero image and modify to test for mobile.

--- js/scripts.js
+++ js/scripts.js
@@ -2,12 +2,12 @@ $(document).ready(function() {
   
   $(window).scroll(function () { 
       //console.log($(window).scrollTop())
-    if ($(window).scrollTop() > 799) {
+    if ($(window).scrollTop() > 999) {
       $('#navbar').addClass('sticky');
       $('body').css('padding-top', '8.25vw'); 
       $('#navmargin').addClass('topblock');
     }
-    if ($(window).scrollTop() < 800) {
+    if ($(window).scrollTop() < 1000) {
       $('#navbar').removeClass('sticky');
       $('body').css('padding-top', '');
       $('#navmargin').removeClass('topblock');
